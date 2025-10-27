c70f9e1208adf2100eba4a51c00210e685e6fba4
s-hashiba <hashiba.suguru.0813@gmail.com>
2025-10-28 00:23:40 +0900

    jQuery実践課題② トップへ戻るボタンの実装

--- js/script.js
+++ js/script.js
@@ -2,3 +2,25 @@
 jQuery(".menu-toggle").on("click", function(){
     jQuery("#menu-drawer").slideToggle();
 });
+
+// トップに戻るボタンの実装
+$(function () {
+  const pageTop = $("#page-top");
+  pageTop.hide(); // 最初はボタンを非表示にする
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 80) { // 80pxスクロールしたら表示
+      pageTop.fadeIn(); // 80px以上スクロールしたらボタンをフェードイン
+    } else {
+      pageTop.fadeOut(); // 80px以下になったらボタンをフェードアウト
+    }
+  });
+  pageTop.click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0, // 上から0pxの位置に戻る
+      },
+      500 // 500ミリ秒かけて戻る
+    );
+    return false;
+  });
+});
